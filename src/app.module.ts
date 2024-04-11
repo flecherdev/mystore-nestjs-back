4e0951c8e8e643e164b07465fbeb02731274e058
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2024-04-10 21:41:05 -0300

    added global module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,24 @@
-import { HttpService, Module, HttpModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './products/product.module';
 import { UsersModule } from './users/users.module';
-
-const API_KEY = '12345634';
-const API_KEY_PROD = 'PROD1212121SA';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ProductModule, UsersModule, HttpModule],
+  imports: [ProductModule, UsersModule, HttpModule, DatabaseModule],
   controllers: [AppController],
   providers: [
     AppService,
     {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
-      // useValue: API_KEY,
-    },
-    {
       provide: 'TASKS',
       useFactory: async (http: HttpService) => {
-        const tasks = await http
-          .get('http://jsonplaceholder.typicode.com/todos')
-          .toPromise();
-        return tasks.data;
+        const request = await http.get(
+          'http://jsonplaceholder.typicode.com/todos',
+        );
+
+        return request;
       },
       inject: [HttpService],
     },
