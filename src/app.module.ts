6c6ff545c9c957deb98ab0a7b35bbcf8bab463cb
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2024-04-17 11:32:40 -0300

    added joi to validation env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './products/product.module';
 import { UsersModule } from './users/users.module';
-import { HttpModule, HttpService } from '@nestjs/axios';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
-import { environments } from '../environments';
+
+import { environments } from './environments';
+import config from './config';
 
 @Module({
   imports: [
@@ -16,7 +19,13 @@ import { environments } from '../environments';
     DatabaseModule,
     ConfigModule.forRoot({
       envFilePath: environments[process.env.NODE_ENV] || '.env',
+      load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required,
+        DATABASE_NAME: Joi.string().required,
+        DATABASE_PORT: Joi.number().required,
+      }),
     }),
   ],
   controllers: [AppController],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,15 @@
 import { Inject, Injectable } from '@nestjs/common';
-
+import { ConfigType } from '@nestjs/config';
+import config from './config';
 @Injectable()
 export class AppService {
-  constructor(@Inject('API_KEY') private apiKey: string) {}
+  constructor(
+    @Inject('API_KEY') private apiKey: string,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
   getHello(): string {
+    const apikey = this.configService.apiKey;
+    const dbName = this.configService.database;
     return `Hello World! ${this.apiKey}`;
   }
 }
